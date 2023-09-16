bf623ed8303147fd9c794906bddfa6cb225e62ce
Arpit vashishtha <arpitvashishtha12@gmail.com>
2023-09-16 09:10:46 +0530

    hosting

--- js/typing.js
+++ js/typing.js
@@ -1,15 +1,15 @@
-// let title = document.querySelector('.tt-h2');
-// let name = "Welcome! You come at a right path"
-// let index = 1;
+let title = document.querySelector('.tt-h2');
+let name = "Welcome! You come at a right path"
+let index = 1;
 
-// const typeWriter = () => {
-//     let new_title = name.slice(0,index);
-//     title.innerText = new_title;
+const typeWriter = () => {
+    let new_title = name.slice(0,index);
+    title.innerText = new_title;
 
-//     index > name.length ? index = 1 : index++;
-//     // index++;
+    index > name.length ? index = 1 : index++;
+    // index++;
 
-//     setTimeout(() => typeWriter(), 100)
-// }
+    setTimeout(() => typeWriter(), 100)
+}
 
-// typeWriter();
+typeWriter();
